7d06cd67db49895d3557fa6fec30ba96c1776ce7
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-29 11:28:52 +0100

    updated the failing test

--- __tests__/pages/Home.test.tsx
+++ __tests__/pages/Home.test.tsx
@@ -1,10 +1,15 @@
-import Home from '@/app/page';
+import React, { lazy } from 'react';
 import { render, screen } from '@testing-library/react';
 
 describe('Home page', () => {
-  it('shows the Home page in the document', () => {
+  it('shows the Home page in the document', async () => {
+    const Home = lazy(() => import('@/app/page'));
+
     render(<Home />);
-    const homePage = screen.getByTestId('home_page');
+
+    // I used findByTestId because it is async
+    const homePage = await screen.findByTestId('home_page');
+
     expect(homePage).toBeInTheDocument();
   });
 });
